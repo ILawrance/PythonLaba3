92e274265c350357b67a14c8a5a6d5bd094073d4
Igor_Balashov <wowzawow@list.ru>
2023-05-09 20:43:43 +0300

    Umnojenie

--- laba3Umnojenie.py
+++ laba3Umnojenie.py
@@ -1,3 +1,14 @@
+a = [[2, 4.1, 2], [4, -1, 6.5]]
+n = 2
+m = 3
+z = float(input("z= "))
 
-def Umnojenie():
-    
+
+def Umnojenie(a, n, m, z):
+    for i in range(n):
+        for j in range(m):
+            a[i][j] = a[i][j]*z
+    print(a)
+
+
+Umnojenie(a, n, m, z)
